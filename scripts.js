e6dfa9c11766efbcc2e6bc011383ebaeacf181ce
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-05-09 16:24:40 -0700

    adding two files

--- scripts.js
+++ scripts.js
@@ -1,4 +1,38 @@
 // JavaScript to be added
-var button = document.getElementById("button-yellow");
 
-button.addEventListener("click", prompt("Your Email"));
+window.onload = function() {
+
+
+var button = document.getElementById("button-yellow");
+button.addEventListener("click", function () {
+	prompt("Email please: ");
+	button.innerHTML = "Thank You!";
+
+});
+
+};
+
+
+
+
+
+
+// function email {
+// var thankYou = "Thanks for your email!";
+// var button = document.getElementById("button-yellow");
+// var emailRequest = button.addEventListener("click", prompt("Email please: "));
+// };
+
+
+
+// // JavaScript to be added
+// function email () {
+// var thankYou = "Thanks for your email!";
+// var button = document.getElementById("button-yellow");
+// var emailRequest = button.addEventListener("click", prompt("Email please: "));
+// 	if (emailRequest != undefined || null) {
+//  		button.innerText(thankYou);
+//  		// var response = emailRequest.addEventListener("click", thankYou).innerContent;
+// }
+// };
+
